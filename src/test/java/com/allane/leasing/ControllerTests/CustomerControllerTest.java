c4ee5b7c851d4ab4ab2b44f96a99209f50d66c97
Mohan Prabhakaran <mohandeepak94@gmail.com>
2022-10-03 20:31:34 +0200

    adding controller and service layer tests for customer entity

--- src/test/java/com/allane/leasing/ControllerTests/CustomerControllerTest.java
+++ src/test/java/com/allane/leasing/ControllerTests/CustomerControllerTest.java
@@ -1,4 +1,123 @@
 package com.allane.leasing.ControllerTests;
 
+import com.allane.leasing.controller.CustomerController;
+import com.allane.leasing.model.Customer;
+import com.allane.leasing.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CustomerController.class)
 public class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    private Customer customer1, customer2, customer3;
+
+    @BeforeEach
+    void setUp() {
+        customer1 = new Customer(555L, "Mohan", "Prabhakaran", LocalDate.of(1994, 12, 24));
+        customer2 = new Customer(556L, "John", "Washington", LocalDate.of(1970, 3, 10));
+        customer3 = new Customer(557L, "Amy", "Jackson", LocalDate.of(1990, 9, 10));
+    }
+
+    @Test
+    void findAllCustomers() throws Exception {
+        when(customerService
+                .findAllCustomers())
+                .thenReturn(List.of(customer1, customer2, customer3));
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/customers/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(3))
+                .andExpect(jsonPath("$[0].customerId").value(555L))
+                .andExpect(jsonPath("$[1].customerId").value(556L))
+                .andExpect(jsonPath("$[2].customerId").value(557L));
+    }
+
+    @Test
+    void findCustomerById() throws Exception {
+        when(customerService
+                .findCustomerById(557L))
+                .thenReturn(customer3);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/customers/{id}", 557L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Amy"))
+                .andExpect(jsonPath("$.lastName").value("Jackson"));
+    }
+
+    @Test
+    void saveCustomer() throws Exception {
+        when(customerService.saveCustomer(any()))
+                .thenReturn(customer1);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/customers/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.valueOf(new ObjectMapper().writeValueAsString(customer1))))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.customerId").value(555L))
+                .andExpect(jsonPath("$.firstName").value("Mohan"))
+                .andExpect(jsonPath("$.lastName").value("Prabhakaran"));
+    }
+
+    @Test
+    void updateCustomer() throws Exception {
+        when(customerService.updateCustomer(any(), eq(556L)))
+                .thenReturn(customer2);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.put("/customers/{id}", 556L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.valueOf(new ObjectMapper().writeValueAsString(customer2))))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Washington"));
+    }
+
+    @Test
+    void deleteCustomerById() throws Exception {
+        when(customerService.saveCustomer(any()))
+                .thenReturn(customer3);
+
+        this.mockMvc.perform(delete("/customers/{id}", 557L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
 }
--- src/test/java/com/allane/leasing/ServiceTests/CustomerServiceTest.java
+++ src/test/java/com/allane/leasing/ServiceTests/CustomerServiceTest.java
@@ -1,4 +1,91 @@
 package com.allane.leasing.ServiceTests;
 
-public class CustomerServiceTest {
+import com.allane.leasing.model.Customer;
+import com.allane.leasing.repository.CustomerRepo;
+import com.allane.leasing.service.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class CustomerServiceTest {
+
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private CustomerRepo customerRepository;
+
+    private Customer customer1, customer2, customer3;
+
+    @BeforeEach
+    void setUp() {
+        customer1 = new Customer(555L, "Mohan", "Prabhakaran", LocalDate.of(1994, 12, 24));
+        customer2 = new Customer(556L, "John", "Washington", LocalDate.of(1970, 3, 10));
+        customer3 = new Customer(557L, "Amy", "Jackson", LocalDate.of(1990, 9, 10));
+    }
+
+    @Test
+    void findAllCustomer() {
+        List<Customer> list = new ArrayList<>();
+        list.add(customer1);
+        list.add(customer2);
+        list.add(customer3);
+
+        when(customerRepository.findAll()).thenReturn(list);
+        List<Customer> result = customerService.findAllCustomers();
+
+        assertEquals(3, result.size());
+        verify(customerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void findCustomerById() {
+        when(customerRepository.findById(customer1.getCustomerId())).thenReturn(Optional.of(customer1));
+        Customer actualCustomer = customerService.findCustomerById(customer1.getCustomerId());
+
+        assertEquals(555L, actualCustomer.getCustomerId());
+        assertEquals("Mohan", actualCustomer.getFirstName());
+        assertEquals("Prabhakaran", actualCustomer.getLastName());
+        verify(customerRepository, times(1)).findById(actualCustomer.getCustomerId());
+    }
+
+    @Test
+    void saveCustomer() {
+        when(customerRepository.save(customer1)).thenReturn(customer1);
+
+        assertThat(customerService.saveCustomer(customer1)).isEqualTo(customer1);
+        verify(customerRepository, times(1)).save(customer1);
+    }
+
+    @Test
+    void updateCustomer() {
+        customer1.setFirstName("Mohan");
+        customer1.setLastName("P");
+        when(customerRepository.findById(any())).thenReturn(Optional.of(customer1));
+        when(customerRepository.save(customer1)).thenReturn(customer1);
+        Customer areaUpdated = customerService.updateCustomer(customer1, 555L);
+        assertEquals("Mohan", areaUpdated.getFirstName());
+        assertEquals("P", areaUpdated.getLastName());
+        verify(customerRepository, times(1)).save(customer1);
+    }
+
+
 }
